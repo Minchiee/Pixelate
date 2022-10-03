7fdf64dee72221540a8ae54c79f0d2817d8bff6f
Bella <Isabellapinkerman@gmail.com>
2022-10-03 11:35:07 -0400

    add color to click

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Your code here
 const table =  document.getElementsByTagName('table')[0]
-const button = document.getElementsById('add-row')
+const button = document.getElementById('add-row')
+const select = document.getElementsByTagName('select')[0]
+let defaultColor = 'red'
 
 function makeRow(){
     const tr = document.createElement('tr')
@@ -11,4 +13,20 @@ function makeRow(){
     table.appendChild(tr)
 }
 
-button.addEventListener('click', makeRow())
+function colorize(event){
+    let target = event.target
+    if(target.className.length){
+        target.className = ''
+    }else{
+        target.className = defaultColor
+    }
+}
+
+
+button.addEventListener('click', makeRow)
+
+table.addEventListener('click', colorize)
+
+select.addEventListener("change", (event) =>{
+    defaultColor = event.target.value
+})
