c7691124927cf57b59c81697b3d81efc300be7e3
Minh Nguyen <lambandwoof@gmail.com>
2022-10-03 09:56:06 -0500

    makeRow() function

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Your code here
+const table =  document.getElementsByTagName('table')[0]
+const button = document.getElementsById('add-row')
+
+function makeRow(){
+    const tr = document.createElement('tr')
+    for (let i = 0; i < 20; i++){
+        const td = document.createElement('td')
+        tr.appendChild(td)
+    }
+    table.appendChild(tr)
+}
+
+button.addEventListener('click', makeRow())
